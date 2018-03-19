82f776ae48dc66f95ae9bd008138396ec1613913
isaacbcampos <35542950+isaacbcampos@users.noreply.github.com>
2018-03-19 06:00:42 -0300

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,7 @@
+x <- read.csv("household_power_consumption.txt", header = T, sep = ';', na.strings = "?", nrows = 2075259, check.names = F, stringsAsFactors = F, comment.char = "", quote = '\"')
+View(x)
+y <- subset(x, Date %in% c("1/2/2007", "2/2/2007"))
+y$Date <- as.Date(y$Date, format = "%d/%m/%y")
+hist(y$Global_active_power, xlab = "Global active power(kilowatts)", main = "Global active power", col = "RED")
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
